6a86922a276652c16ac29bdbf87cb768bc7bf1af
github username <joshmrallen@gmail.com>
2020-08-28 23:10:36 -0400

    Done.

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const modal = document.querySelector('#modal')
+modal.className = 'hidden'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+
+  document.addEventListener("click", (e) => {
+
+    if (e.target.closest('.like-glyph')){
+      console.log("Heart Clicked!")
+      
+      //like is red, make clear else make red
+      if (e.target.classList[1] === 'activated-heart'){
+        e.target.innerText = EMPTY_HEART
+        e.target.classList.remove('activated-heart')
+      } else {
+          mimicServerCall()
+          .then(res => {
+            //make the heart red
+            e.target.innerText = FULL_HEART
+            e.target.classList.add('activated-heart')
+          })
+          .catch((error) => {
+            modal.style.visibility = 'visible'
+            setTimeout(function(){
+              modal.style.visibility = 'hidden'
+            }, 5000)
+          })
+      }
+    }
+  })
+
+  
+
+})
+
 
 
 
